6ad9e60407ff89e36a22f82bd1e89a552c388f6f
amiraadev <allagui_fawzi@outlook.fr>
2024-06-27 17:52:27 +0100

    done with the profile.service.ts

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateProfileDto } from './profile.dto';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createProfile(createProfileDto:CreateProfileDto){
+    const profile = await this.prisma.profile.findUnique({
+        where:{email:createProfileDto.email}
+    });
+    if(profile){
+        return profile
+    }
+    return this.prisma.profile.create({
+        data:createProfileDto
+    })
+
+  }
+
+  async getProfileById(id:number){
+    return this.prisma.profile.findUnique({
+        where:{id},
+        include:{
+            servers:{
+                include:{
+                    channels:true
+                }
+            }
+        }
+    })
+  }
+
+  async getProfileByEmail(email:string){
+    return this.prisma.profile.findUnique({
+        where:{email},
+        include:{
+            servers:{
+                include:{
+                    channels:true
+                }
+            }
+        }
+    })
+  }
+
+
+}
