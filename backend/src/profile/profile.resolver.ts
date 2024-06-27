e7fb2043b891ce6bc65c85ca999f5ece3055cafe
amiraadev <allagui_fawzi@outlook.fr>
2024-06-27 21:34:07 +0100

    done with the graphql schema

--- backend/src/profile/profile.resolver.ts
+++ backend/src/profile/profile.resolver.ts
@@ -2,11 +2,14 @@ import { Args, Mutation, Resolver,Query } from '@nestjs/graphql';
 import { Profile } from './profile.types';
 import { CreateProfileDto } from './profile.dto';
 import { ProfileService } from './profile.service';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class ProfileResolver {
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(GraphqlAuthGuard)
   @Mutation(() => Profile)
   async createProfile(@Args('input') input: CreateProfileDto) {
     return this.profileService.createProfile(input);
