951b46c3f363e6286f28830fb070219091155276
amiraadev <allagui_fawzi@outlook.fr>
2024-06-27 15:18:08 +0100

    setup of the GraphQL in nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname, '..',"public"),
+      serveRoot:'/'
+    }),
+    GraphQLModule.forRootAsync({
+      imports:[],
+      inject:[],
+      driver:ApolloDriver,
+      useFactory:async() =>{
+        playground:true;
+        return {
+          autoSchemaFile:join(process.cwd(),'src/schema.gql'),
+          sortSchema:true,
+          subscriptions:{}
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
