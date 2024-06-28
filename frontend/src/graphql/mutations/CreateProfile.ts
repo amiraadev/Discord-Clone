0e4cf7af6a23daa41387cde5f41e2c1aed178073
amiraadev <allagui_fawzi@outlook.fr>
2024-06-28 10:56:18 +0100

    create the createProfile mutation

---
+++ frontend/src/graphql/mutations/CreateProfile.ts
@@ -0,0 +1,14 @@
+/** @format */
+
+import { gql } from "@apollo/client";
+
+export const CREATE_PROFILE = gql`
+	mutation createProfile($input: CreateProfileDto!) {
+		createProfile(input: $input) {
+			id
+			imageUrl
+			name
+			email
+		}
+	}
+`;
